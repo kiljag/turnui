42d73ec6242b593a0c815b58a91bd0ecf0f61375
Jagadeesh Killi <kjaga12dev@gmail.com>
2023-06-05 23:00:22 +0530

    delay localstream

--- src/components/chess/ChessApp.tsx
+++ src/components/chess/ChessApp.tsx
@@ -19,7 +19,10 @@ export default function ChessApp() {
     }
 
     useEffect(() => {
-        setupLocalStream();
+        setTimeout(() => {
+            setupLocalStream();
+        }, 2000);
+
     })
 
     return (
